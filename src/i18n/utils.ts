be588bcc82e3aba5c38567f1f81c29b59de24663
Joseth <jojo.programmer01@gmail.com>
2024-12-07 00:37:48 +0000

    update the LanguagePicker to donta animate

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang, routes, showDefaultLang } from './ui';
+import { ui, defaultLang, showDefaultLang, languages } from './ui';
 
 export function getLangFromUrl(url: URL) {
     const [, lang] = url.pathname.split('/');
@@ -12,9 +12,17 @@ export function useTranslations(lang: keyof typeof ui) {
     };
 }
 
-export function useTranslatedPath(currentLang: keyof typeof ui) {
+/* export function useTranslatedPath(currentLang: keyof typeof ui) {
     return function translatePath(path: string, targetLang: string = currentLang) {
         const cleanPath = path.startsWith("/") ? path : `/${path}`;
         return targetLang === defaultLang ? cleanPath : `/${targetLang}${cleanPath}`;
+    };
+} */
+
+export function useTranslatedPath(currentLang: keyof typeof languages) {
+    return function translatePath(currentPath: string, targetLang: string = currentLang) {
+        // Substitui o idioma atual pelo novo idioma
+        const newPath = currentPath.replace(`/${currentLang}`, `/${targetLang}`);
+        return newPath;
     };
 }
