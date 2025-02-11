9e9c973b280169c5e3c516635291085915ec02af
Joseth <jojo.programmer01@gmail.com>
2025-02-11 01:17:47 +0000

    fix the shit of pnpm-lock.yaml and preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import preact from '@astrojs/preact';
 
 import node from '@astrojs/node';
+
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
